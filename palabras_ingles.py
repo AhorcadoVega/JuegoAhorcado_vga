3fff6630ba7b6af6b48a5cfa833020a455cad81f
AhorcadoVega <lolorv15nova@gmail.com>
2024-01-21 22:31:14 -0600

    Se agrega comentario en palabras_ingles

--- palabras_ingles.py
+++ palabras_ingles.py
@@ -1,3 +1,7 @@
+"""Aqui puedes agregar más palabras actualmente la lista contiene 110 palabras en ingles
+    puedes agregar más y de mayor dificultad o más especificas por el momento tendria que ser de 
+    manera manual """
+
 palabras = [
     "About", "Add", "Activity", "Actually", "Above", "The", "And", "You", "That", "Was", "For", "Are", "Dog", "Cat", "Ball", "Tree", "House", "Car", "Sun", "Moon", "Rain", "Book", "Chair", "Cake", "Bird", "Fish", "Jump", "Run", "Smile", "Friend", "Happy", "Sad", "Blue", "Red", "Green", "Yellow", "Orange", "Big", "Small", "Hot", "Cold", "Eat", "Drink", "Sleep", "Wake", "Talk", "Listen", "Read", "Write", "Play", "Sing", "Dance", "Laugh", "Cry", "Think", "Walk", "Stop", "Go", "See", "Hear", "Touch", "Smell", "Taste", "Fast", "Slow", "Down", "Left", "Right", "Love", "Hate", "Day", "Night", "Star", "Flower", "Ocean", "Mountain", "River", "Snow", "Ice", "Fire", "Wind", "Thunder", "Lightning", "Cloud", "Rainbow", "Butterfly", "Spider", "Bee", "Elephant", "Lion", "Tiger", "Bear", "Monkey", "Frog", "Turtle", "Snake", "Rabbit", "Horse", "Duck", "Chicken", "Milk", "Bread", "Juice", "Body", "Choice", "Communication",
     "Hour", "information", "Know", "Myself"]
